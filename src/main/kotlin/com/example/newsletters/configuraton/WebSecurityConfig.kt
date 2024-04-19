17e58f5dcaaf72f1ae6f7940c6e5962698aecd5e
atyutin <tutin.andery@gmail.com>
2024-04-20 00:47:30 +0300

    security

---
+++ src/main/kotlin/com/example/newsletters/configuraton/WebSecurityConfig.kt
@@ -0,0 +1,49 @@
+package com.example.newsletters.configuraton
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer.AuthorizationManagerRequestMatcherRegistry
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig {
+    @Bean
+    @Throws(Exception::class)
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests {
+                it
+                    .requestMatchers( "/css/**", "/images/**", "/js/**", "/webjars/**").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .formLogin {
+                it
+                    .loginPage("/login")
+                    .permitAll()
+            }
+            .logout { it.permitAll() }
+
+        return http.build()
+    }
+
+    @Bean
+    fun userDetailsService(): UserDetailsService {
+        val user: UserDetails = User.withDefaultPasswordEncoder()
+                .username("u")
+                .password("1")
+                .build()
+
+        return InMemoryUserDetailsManager(user)
+    }
+}
